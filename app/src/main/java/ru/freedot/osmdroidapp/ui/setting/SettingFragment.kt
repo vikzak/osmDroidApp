f533184c8176e223a7e046ddde83bc468849bb40
vikzak <git@azbukait.ru>
2022-07-06 23:52:50 +0300

    osm droid app

---
+++ app/src/main/java/ru/freedot/osmdroidapp/ui/setting/SettingFragment.kt
@@ -0,0 +1,41 @@
+package ru.freedot.osmdroidapp.ui.setting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ru.freedot.osmdroidapp.databinding.FragmentSettingBinding
+
+class SettingFragment : Fragment() {
+
+    private var _binding: FragmentSettingBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val settingViewModel =
+            ViewModelProvider(this).get(SettingViewModel::class.java)
+
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textNotifications
+//        notificationsViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
